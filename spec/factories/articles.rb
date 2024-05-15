7799f9b85a1d57b532f06e05c2d7a1847108d093
nnc_kasano_keisuke <nnc-194@nintendo.co.jp>
2024-05-15 16:24:23 +0900

    factory_bot after build

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,9 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :article do
+
+    after(:build) do |article|
+      article.image.attach(io: File.open('spec/fixtures/sample.png'), filename: 'sample.png', content_type: 'image/png')
+    end
   end
 end
